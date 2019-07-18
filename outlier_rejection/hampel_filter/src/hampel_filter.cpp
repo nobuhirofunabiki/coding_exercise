e42ffedf36f04d4660b058a94dc92fc2ec03dd9a
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-07-17 20:42:24 -0700

    Baseline is completed

--- outlier_rejection/hampel_filter/src/hampel_filter.cpp
+++ outlier_rejection/hampel_filter/src/hampel_filter.cpp
@@ -1,68 +1,99 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 #include <vector>
 
 void hampelOutlierRejection(std::vector<double> data_input, 
-                            std::vector<bool> b_outlier_list);
+                            std::vector<double> data_stamp,
+                            std::vector<bool>& b_outlier_list);
+void calculateLocalWindow(std::vector<double> data_input, std::vector<double> data_stamp,
+                          double dx, unsigned int data_index,
+                          std::vector<double>& local_ref, std::vector<double>& local_var);
 double calculateMedian(std::vector<double> data_input);
 
 int main(){
-    std::vector<double> data_input {5.0, 4.0, 60.0, 4.5, 5.5, 5.2, 6.0};
+    std::vector<double> data_stamp {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<double> data_input {5.0, 4.0, 60.0, 4.5, 5.5, 5.2, 6.0, 70, 5.8};
     std::vector<bool> b_outlier_list(data_input.size(), false);
 
-    hampelOutlierRejection(data_input, b_outlier_list);
+    hampelOutlierRejection(data_input, data_stamp, b_outlier_list);
+
+    std::cout << "outlier list" << "\n";
+    for (auto iOut : b_outlier_list) {
+        std::cout << iOut << " ";
+    }
 }
 
 
 void hampelOutlierRejection(std::vector<double> data_input, 
-                            std::vector<bool> b_outlier_list) {
+                            std::vector<double> data_stamp,
+                            std::vector<bool>& b_outlier_list) {
     unsigned int data_size = data_input.size();
 
-    std::vector<double> sorted_data = data_input;
+    std::vector<double> sorted_data  = data_input;
+    std::vector<double> sorted_stamp = data_stamp;
     std::sort(sorted_data.begin(), sorted_data.end());
-
-    // for (auto itr : sorted_data) {
-    //     std::cout << itr << " ";
-    // }
-    // std::cout << "\n";
+    std::sort(sorted_stamp.begin(), sorted_stamp.end());
 
     double median_value;
     median_value = calculateMedian(sorted_data);
-    // std::cout << "Median value is " << median_value << std::endl;
+    double median_stamp;
+    median_stamp = calculateMedian(sorted_stamp);
 
-    std::vector<double> data_temp1 = sorted_data;
-    std::vector<double> data_temp2 = sorted_data;
+    std::vector<double> data_temp1 = sorted_stamp;
+    std::vector<double> data_temp2 = sorted_stamp;
     std::vector<double> data_temp3;
     data_temp1.erase(data_temp1.begin());
     data_temp2.pop_back();
-
-    // for (auto itr : data_temp1) {
-    //     std::cout << itr << " ";
-    // }
-    // std::cout << "\n";
-
-    // for (auto itr : data_temp2) {
-    //     std::cout << itr << " ";
-    // }
-    // std::cout << "\n";
 
     for (int iData = 0; iData < data_temp1.size(); iData++) {
         data_temp3.push_back(data_temp1[iData] - data_temp2[iData]);
     }
 
-    // for (auto itr : data_temp3) {
-    //     std::cout << itr << " ";
-    // }
-    // std::cout << "\n";
-
     double dx = 3.0*calculateMedian(data_temp3);
-    // std::cout << "dx is " << dx << "\n";
 
-    // for (auto itr : b_outlier_list) {
-    //     std::cout << itr << " ";
-    // }
-    // std::cout << "\n";
+    std::vector<double> local_ref;
+    std::vector<double> local_var;
 
+    for (int iData = 0; iData < data_size; iData++) {
+        calculateLocalWindow(data_input, data_stamp, dx, iData, local_ref, local_var);
+    }
+
+    for (int iData = 0; iData < data_size; iData++) {
+        double temp = std::fabs(data_input[iData] - local_ref[iData]) - 3.0*local_var[iData];
+        if (temp > 0) {
+            b_outlier_list[iData] = true;
+        }
+    }
+}
+
+void calculateLocalWindow(std::vector<double> data_input, std::vector<double> data_stamp,
+                          double dx, unsigned int data_index,
+                          std::vector<double>& local_ref, std::vector<double>& local_var) {
+
+    // Index related to local window
+    std::vector<int> index_local;
+    std::vector<double> data_in_window;
+    int data_size = data_stamp.size();
+    for (int iData = 0; iData < data_size; iData++) {
+        if (data_stamp[data_index] - dx <= data_stamp[iData]
+            && data_stamp[data_index] + dx >= data_stamp[iData]) {
+            index_local.push_back(iData);
+            data_in_window.push_back(data_input[iData]);
+        }
+    }
+
+    // Calculate local nominal data reference value
+    double local_ref_temp = calculateMedian(data_in_window);
+    local_ref.push_back(local_ref_temp);
+
+    std::vector<double> diff_ref;
+    for (int iData = 0; iData < data_in_window.size(); iData++) {
+        diff_ref.push_back(std::fabs(data_in_window[iData] - local_ref_temp));
+    }
+
+    // Calculate local scale of natural variation
+    local_var.push_back(1.4826*calculateMedian(diff_ref));
 }
 
 double calculateMedian(std::vector<double> data_input) {
