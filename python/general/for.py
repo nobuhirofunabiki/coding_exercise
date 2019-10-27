39047cc274424f1d8966252be931129beec10bdb
Nobuhiro Funabiki <nobuhiro.funabiki@jpl.nasa.gov>
2019-10-26 22:12:45 -0700

    Add a simple python cord of 'for' syntax

---
+++ python/general/for.py
@@ -0,0 +1,19 @@
+print('for: simple example')
+words = ['a', 'b', 'c']
+for iWords in words:
+    print(iWords)
+print('\n')
+
+print('for: range function-1')
+for iNum in range(5):
+    print(iNum)
+print('\n')
+
+print('for: range function-2')
+for iNum in range(3, 6):
+    print(iNum)
+print('\n')
+
+print('for: range function-3')
+for iNum in range(0, 7, 2):
+    print(iNum)
