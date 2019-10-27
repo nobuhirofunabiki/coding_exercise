abd155480ea1c612a1a0325345b305fe61ad31b8
Nobuhiro Funabiki <nobuhiro.funabiki@jpl.nasa.gov>
2019-10-26 22:13:31 -0700

    Add a simple python cord of 'in' and 'not in' syntaxes

---
+++ python/general/in_notin.py
@@ -0,0 +1,17 @@
+if 'p' in 'python':
+    print('Included')
+else:
+    print('Not included')
+print('\n')
+
+if 'p' not in 'python':
+    print('Not included')
+else:
+    print('Included')
+print('\n')
+
+data = [1, 2, 3, 4, 5]
+if 6 in data:
+    print('6 is in the data list')
+else:
+    print('6 is not in the data list')
