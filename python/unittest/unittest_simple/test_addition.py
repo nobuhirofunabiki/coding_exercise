39034d7ece97ae452ed4c1e5b1495e7f0b9e59e0
Nobuhiro Funabiki <nobuhiro.funabiki@jpl.nasa.gov>
2019-10-26 20:55:38 -0700

    Add a simple unittest for python

---
+++ python/unittest/unittest_simple/test_addition.py
@@ -0,0 +1,13 @@
+import unittest
+from addition import addition
+
+class TestAddition(unittest.TestCase):
+    def test_addition(self):
+        value1 = 2
+        value2 = 3
+        expected = 6
+        actual = addition(value1, value2)
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    unittest.main()
