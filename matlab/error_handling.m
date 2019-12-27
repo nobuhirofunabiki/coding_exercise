9d5ddcb418548e4acc9298a9859b5219f8f65d0b
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-12-26 20:22:20 -0800

    [MATLAB] modify the error handling

--- matlab/error_handling.m
+++ matlab/error_handling.m
@@ -3,7 +3,7 @@ clear
 close all;
 restoredefaultpath
 
-m = 1;
+m = 2;
 if (m ~= 2)
     % Throw error and display message
     % error("m is not 1");
@@ -13,3 +13,7 @@ disp("pass error");
 
 % Throw error if the condition is false
 assert(m == 2, "m is not 1");
+
+A = [0 0; 1 1];
+size_A = size(A);
+assert(isequal(size(A), [2 1]), "A is NOT correct size matrix");
