9bfc9da288ba1e66d86cd3a863d51b201a58bad4
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-12-27 21:13:43 -0800

    [MATLAB] Add a few reference URLs for diamond_problem.m

--- matlab/class/diamond_problem/main.m
+++ matlab/class/diamond_problem/main.m
@@ -1,3 +1,7 @@
+% (references)
+% https://www.mathworks.com/matlabcentral/answers/93189-how-does-matlab-solve-the-diamond-problem-in-multiple-inheritance
+% https://blogs.mathworks.com/developer/2015/06/08/diamond-of-death/
+
 clc
 clear
 close all
